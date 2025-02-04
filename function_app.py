d7c6db30f3f26b00379f76da129945ead9353222
NathanAzvdo <nathan.azevedo28@gmail.com>
2025-02-04 18:31:16 -0300

    feature: loading page with bs4

--- function_app.py
+++ function_app.py
@@ -1,6 +1,18 @@
 import azure.functions as func
-import datetime
-import json
 import logging
+import os
+import requests
+from bs4 import BeautifulSoup
 
 app = func.FunctionApp()
+
+@app.route(route="SapiensiaCrawler", auth_level=func.AuthLevel.ANONYMOUS)
+def SapiensiaCrawler(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    base_url = "https://sapiensia.atlassian.net/wiki/spaces/SIA/overview?homepageId=578912377"
+    requisition_response = requests.get(base_url)
+    soup = BeautifulSoup(requisition_response.content, 'html.parser')
+    
+
+    return func.HttpResponse(str(soup), mimetype="text/html")
